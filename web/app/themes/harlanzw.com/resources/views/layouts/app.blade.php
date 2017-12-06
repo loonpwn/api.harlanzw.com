fe64d2596108fe4cbe0c6281ba7e3747d5f8d4cb
Harlan Wilton <harlan.wilton@4mation.com.au>
2017-12-07 00:33:21 +1100

    master: moving around gtm

--- web/app/themes/harlanzw.com/resources/views/layouts/app.blade.php
+++ web/app/themes/harlanzw.com/resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
   @include('partials.head')
   <body @php(body_class())>
 
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRTRSL7"
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T573KVJ"
                     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+  <!-- End Google Tag Manager (noscript) -->
 
     @php(do_action('get_header'))
     @include('partials.header')
