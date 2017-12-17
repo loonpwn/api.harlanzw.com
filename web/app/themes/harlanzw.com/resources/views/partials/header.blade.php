0709523fda487d0928264d0036d00036c3834c75
Harlan Wilton <harlan.wilton@4mation.com.au>
2017-12-17 11:24:12 +1100

    master: adding material components

--- web/app/themes/harlanzw.com/resources/views/partials/header.blade.php
+++ web/app/themes/harlanzw.com/resources/views/partials/header.blade.php
@@ -1,10 +1,10 @@
-<header class="banner">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <a class="navbar-brand" href="/">HarlanZW</a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
   </div>
-</header>
+</nav>
