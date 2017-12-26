33bf9f5e82e49aadfb3f126b501b03ee9ea79ec3
Harlan Wilton <harlan.wilton@4mation.com.au>
2017-12-26 12:55:58 +1100

    master: moving git hooks to prod

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="content-info">
     <div class="container">
         @php(dynamic_sidebar('sidebar-footer'))
+        &copy;  {{ date('Y') }} Harlan Wilton. Photography provided by <a href="https://www.bensanfordmedia.com">Ben Sanford</a>.
     </div>
 </footer>
