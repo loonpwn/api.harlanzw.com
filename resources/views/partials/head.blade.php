6fa4c79281d0de136a87ba089c3f5203206844bc
Harlan Wilton <harlan.wilton@4mation.com.au>
2017-12-26 15:08:30 +1100

    master: adding favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -19,7 +19,6 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta content="initial-scale=1, shrink-to-fit=no, width=device-width" name="viewport">
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Material+Icons" rel="stylesheet">
     @php(wp_head())
 </head>
