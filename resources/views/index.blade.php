bbb45a9e0add417be7f9ec948501cdce922056a0
Harlan Wilton <harlan.wilton@4mation.com.au>
2017-12-30 16:43:52 +1100

    master: making article section bit nicer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('partials.page-header')
+    <section class="section">
 
-    @if (!have_posts())
-        <div class="alert alert-warning">
-            {{ __('Sorry, no results were found.', 'sage') }}
+        <div class="container">
+            @include('partials.page-header')
+
+            @if (!have_posts())
+                <div class="alert alert-warning">
+                    {{ __('Sorry, no results were found.', 'sage') }}
+                </div>
+                {!! get_search_form(false) !!}
+            @endif
+            <div class="row">
+                <div class="col-sm-4">
+                    @while (have_posts()) @php(the_post())
+                    @include('partials.content-'.get_post_type())
+                    @endwhile
+                </div>
+            </div>
+
+            {!! get_the_posts_navigation() !!}
         </div>
-        {!! get_search_form(false) !!}
-    @endif
-
-    @while (have_posts()) @php(the_post())
-    @include('partials.content-'.get_post_type())
-    @endwhile
-
-    {!! get_the_posts_navigation() !!}
+    </section>
 @endsection
