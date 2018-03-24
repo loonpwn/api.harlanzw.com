e9165d8971884007b3a19677057dd3826a1f66fc
Harlan Wilton <harlan.wilton@4mation.com.au>
2018-03-25 01:14:44 +1100

    master: fixing author

--- resources/views/widgets/author/markup.blade.php
+++ resources/views/widgets/author/markup.blade.php
@@ -2,7 +2,7 @@
     <p class="text-center" style="margin-top: 2em;">
         <span class="chip">
                 <a href="/about/">
-                <img alt="Author Harlan Wilton" class="chip-img lazyload" src="{{ \App\asset_path('/images/harlan-wilton.jpg') }}">
+                <img alt="Author Harlan Wilton" class="chip-img lazyload" src="{{ \App\asset_path('images/harlan-wilton.jpg') }}">
                 Harlan Wilton
                     </a>
             </span>
