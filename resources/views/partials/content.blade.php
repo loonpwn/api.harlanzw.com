2d50d4db2e0c695877abc379b6f2a9847e0271b8
Harlan Wilton <harlan.wilton@4mation.com.au>
2018-04-02 11:55:28 +1000

    master: fixing thumbnails

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <a href="{{ get_the_permalink() }}">
-        {!! wp_get_attachment_image(get_post_thumbnail_id(get_the_ID()),  'thumbnail', '', ['class' => 'card-img lazyload']) !!}
+        {!! wp_get_attachment_image(get_post_thumbnail_id(get_the_ID()),  'post-thumbnail', '', ['class' => 'card-img lazyload']) !!}
         <div class="card-body">
             <h4 class="card-title">{{ get_the_title() }}</h4>
         </div>
