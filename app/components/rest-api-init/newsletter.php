fb8cb0bc5a47be23723f0e44592bef31703a80d6
Harlan Wilton <harlan@harlanzw.com>
2018-12-02 11:14:08 +1100

    master: fix newsletter subscription

--- app/components/rest-api-init/newsletter.php
+++ app/components/rest-api-init/newsletter.php
@@ -4,7 +4,7 @@
 register_rest_route('newsletter/v1', '/submission', [
     'methods' => 'POST',
     'callback' => function(WP_REST_Request $request) {
-        wp_mail('harlan@harlanzw.com', 'New Lead: ' . $request->get_body_params()['email'] . ' from ' . $request->get_body_params()['page'], '', '');
+        wp_mail('harlan@harlanzw.com', 'New Lead: ' . $request->get_body_params()['email'] . ' from ' . $request->get_body_params()['page'], print_r($request, true), '');
 
         $response = new WP_REST_Response();
         $response->set_status(201);
