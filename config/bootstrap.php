795da124a474b40186aa96764aabde5c0c4412e0
Harlan Wilton <harlan@harlanzw.com>
2020-02-04 18:04:58 +1100

    Fix config

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -89,7 +89,7 @@ define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
 $base = '/';
-define('DOMAIN_CURRENT_SITE', 'local.harlanzw.com');
+define('DOMAIN_CURRENT_SITE', 'harlanzw.com');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
